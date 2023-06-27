a40bc8e13ba0fcf18ad25fb7e980fc4d53754dce
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-06-27 02:51:33 +0200

    benchmark: Host nanobench benchmark by Catch2

---
+++ benchmark/benchmark.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+/// See https://github.com/martinus/nanobench
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include <nanobench.h>
