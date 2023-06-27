253b05f886c86934e05747139eeeb166352912a8
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-06-27 03:07:18 +0200

    benchmark: Add LRU cache benchmarking

---
+++ benchmark/lc_lru_cache_benchmark.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include <fmt/format.h>
+
+#include "forfun/lc_lru_cache.hpp"
+
+template <typename T>
+std::enable_if_t<std::is_base_of_v<forfun::lrucache::LRUCacheBase, T>, void>
+test(int const capacity) {
+    int volatile val{};
+    T cache(capacity);
+
+    for (int i{0}; i < capacity; ++i) {
+        cache.put(i, i);
+    }
+
+    val = cache.get(1);
+
+    val = cache.get(2);
+
+    val = cache.get(3);
+
+    cache.put(capacity + 1, capacity + 1);
+
+    val = cache.get(1);
+
+    val = cache.get(4);
+
+    val = cache.get(2);
+
+    cache.put(5, 5);
+
+    val = cache.get(3);
+
+    for (int i{0}; i < capacity; ++i) {
+        val = cache.get(i);
+    }
+}
+
+TEST_CASE("forfun::lrucache benchmarking") {
+    SECTION("small") {
+        constexpr int const lrucache_capacity{32};
+
+        ankerl::nanobench::Bench()
+
+            .title(
+                fmt::format("LRU Cache with {} cache items", lrucache_capacity))
+
+            .run(
+                NAMEOF_FULL_TYPE(forfun::lrucache::stl::LRUCache).data(),
+                []() {
+                    test<forfun::lrucache::stl::LRUCache>(lrucache_capacity);
+                })
+
+            .run(
+                NAMEOF_FULL_TYPE(forfun::lrucache::naive::LRUCache).data(),
+                []() {
+                    test<forfun::lrucache::naive::LRUCache>(lrucache_capacity);
+                })
+
+            ;
+    }
+
+    SECTION("large") {
+        constexpr int const lrucache_capacity{128};
+
+        ankerl::nanobench::Bench()
+
+            .title(
+                fmt::format("LRU Cache with {} cache items", lrucache_capacity))
+
+            .run(
+                NAMEOF_FULL_TYPE(forfun::lrucache::stl::LRUCache).data(),
+                []() {
+                    test<forfun::lrucache::stl::LRUCache>(lrucache_capacity);
+                })
+
+            .run(
+                NAMEOF_FULL_TYPE(forfun::lrucache::naive::LRUCache).data(),
+                []() {
+                    test<forfun::lrucache::naive::LRUCache>(lrucache_capacity);
+                })
+
+            ;
+    }
+}
