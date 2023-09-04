12e835c89a720f4b3903a873da3cc9f5476806df
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-09-04 21:49:51 +0200

    test: Add palindromic numbers Catch2 tests

---
+++ test/palindromic_number_test.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include "forfun/palindromic_number.hpp"
+
+TEMPLATE_TEST_CASE_SIG(
+    "palindromic_number", "", ((auto sut), sut),
+    (is_palindrome)) {
+    SECTION("Palindromic numbers") {
+        GIVEN("a palindromic number") {
+            auto const [palindromic_number]{GENERATE(table<int>({
+                {0},
+                {1},
+                {11},
+                {121},
+                {212},
+                {333},
+                {4444},
+                {14341},
+                {143341},
+                {4268624},
+                {1234554321},
+            }))};
+
+            CAPTURE(palindromic_number);
+
+            WHEN("is_palindrome checked") {
+                THEN("true") {
+                    REQUIRE(sut(palindromic_number));
+                }
+            }
+        }
+    }
+
+    SECTION("Non-palindromic numbers") {
+        GIVEN("a non-palindromic number") {
+            auto const [non_palindromic_number]{GENERATE(table<int>({
+                {std::numeric_limits<int>::min()},
+                {-1},
+                {-32},
+                {12},
+                {112},
+                {1121},
+                {12325},
+                {4268604},
+                {std::numeric_limits<int>::max()},
+            }))};
+
+            CAPTURE(non_palindromic_number);
+
+            WHEN("is_palindrome checked") {
+                THEN("false") {
+                    REQUIRE_FALSE(sut(non_palindromic_number));
+                }
+            }
+        }
+    }
+}
