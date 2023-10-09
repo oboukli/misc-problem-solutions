53965f20c9bda28614832a33fb1b6b5c17228181
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-10-09 22:10:17 +0200

    fix: Include correct headers

--- benchmark/palindromic_number_benchmark.cpp
+++ benchmark/palindromic_number_benchmark.cpp
@@ -4,7 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 
-#include <type_traits>
+#include <limits>
 
 #include <catch2/catch_test_macros.hpp>
 
--- test/palindromic_number_test.cpp
+++ test/palindromic_number_test.cpp
@@ -4,6 +4,8 @@
 
 // SPDX-License-Identifier: MIT
 
+#include <limits>
+
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
