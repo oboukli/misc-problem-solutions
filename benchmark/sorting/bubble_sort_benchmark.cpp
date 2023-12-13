1875211799c8586f158659294e1da993a5e8ec40
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-12-13 22:10:17 +0200

    benchmark: Add bubble sort benchmark

---
+++ benchmark/sorting/bubble_sort_benchmark.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <array>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/sorting/bubble_sort.hpp"
+
+TEST_CASE("bubble_sort benchmarking", "[benchmark][sorting][bubble_sort]")
+{
+    using ContainerType = std::array<int, 512>;
+
+    ankerl::nanobench::Bench()
+
+        .title("Bubble sort")
+
+        .run(
+            NAMEOF_RAW(
+                forfun::sorting::plain::bubble_sort<ContainerType::iterator>)
+                .c_str(),
+            []() {
+                ContainerType arr{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+                forfun::sorting::plain::bubble_sort(arr.begin(), arr.end());
+
+                ankerl::nanobench::doNotOptimizeAway(arr);
+            })
+
+        .run(
+            NAMEOF_RAW(
+                forfun::sorting::stl::bubble_sort<ContainerType::iterator>)
+                .c_str(),
+            []() {
+                ContainerType arr{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+                forfun::sorting::stl::bubble_sort(arr.begin(), arr.end());
+
+                ankerl::nanobench::doNotOptimizeAway(arr);
+            });
+}
