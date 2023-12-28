068124b0b42b015edb58f039749ba03eb83c852a
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-12-28 22:10:17 +0200

    optimize: Remove redundant assignment

--- include/forfun/contains_duplicate.hpp
+++ include/forfun/contains_duplicate.hpp
@@ -17,13 +17,13 @@ namespace forfun::contains_duplicate {
 template <typename RandomIt>
     requires std::forward_iterator<RandomIt>
 [[nodiscard]] constexpr inline bool
-contains_duplicate(RandomIt const first, RandomIt const end) noexcept
+contains_duplicate(RandomIt it, RandomIt const end) noexcept
 {
-    for (auto it_i{first}; it_i != end; ++it_i)
+    for (; it != end; ++it)
     {
-        auto const t{*it_i};
+        auto const t{*it};
 
-        for (auto it_j{it_i + 1}; it_j != end; ++it_j)
+        for (auto it_j{it + 1}; it_j != end; ++it_j)
         {
             if ((*it_j) == t)
             {
