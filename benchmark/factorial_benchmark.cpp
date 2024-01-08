c52b454ee590b3461d07164231163a501e6d494b
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-01-08 22:10:17 +0200

    benchmark: Add stl_functional::factorial

--- benchmark/factorial_benchmark.cpp
+++ benchmark/factorial_benchmark.cpp
@@ -17,6 +17,9 @@
 TEST_CASE("factorial benchmarking", "[benchmark][factorial]")
 {
     namespace iterative = forfun::factorial::iterative;
+#if FORFUN_FACTORIAL_STL_FUNCTIONAL_AVAILABLE
+    namespace stl_functional = forfun::factorial::stl_functional;
+#endif // FORFUN_FACTORIAL_STL_FUNCTIONAL_AVAILABLE
 
     ankerl::nanobench::Bench()
 
@@ -33,6 +36,19 @@ TEST_CASE("factorial benchmarking", "[benchmark][factorial]")
 
                 ankerl::nanobench::doNotOptimizeAway(r);
             })
+
+#if FORFUN_FACTORIAL_STL_FUNCTIONAL_AVAILABLE
+        .run(
+            NAMEOF_RAW(stl_functional::factorial<std::uint64_t>).c_str(),
+            []() {
+                auto const volatile n{std::uint64_t{20}};
+                auto const r{forfun::factorial::stl_functional::factorial(n)};
+
+                assert(r == std::uint64_t{2'432'902'008'176'640'000});
+
+                ankerl::nanobench::doNotOptimizeAway(r);
+            })
+#endif // FORFUN_FACTORIAL_STL_FUNCTIONAL_AVAILABLE
 
         ;
 }
