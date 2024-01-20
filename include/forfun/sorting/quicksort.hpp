45ec9b755eb84915de5d120d9e63768e5423c2ab
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-01-20 22:10:17 +0200

    fix: Correct memory access guard

--- include/forfun/sorting/quicksort.hpp
+++ include/forfun/sorting/quicksort.hpp
@@ -42,21 +42,35 @@ partition(Iter const lo, Iter const hi) noexcept
     return i;
 }
 
+template <typename Iter>
+    requires std::contiguous_iterator<Iter>
+constexpr inline void sort(Iter const lo, Iter const hi) noexcept
+{
+    using Diff = std::iterator_traits<Iter>::difference_type;
+
+    if (lo < hi)
+    {
+        auto const p{partition(lo, hi)};
+
+        sort(lo, p - Diff{1});
+        sort(p + Diff{1}, hi);
+    }
+}
+
 } // namespace
 
 template <typename Iter>
     requires std::contiguous_iterator<Iter>
-constexpr inline void quicksort(Iter const lo, Iter const hi) noexcept
+constexpr inline void quicksort(Iter const begin, Iter const end) noexcept
 {
     using Diff = std::iterator_traits<Iter>::difference_type;
 
-    if (hi > lo)
+    if (begin == end)
     {
-        auto const p{partition(lo, hi)};
-
-        quicksort(lo, p - Diff{1});
-        quicksort(p + Diff{1}, hi);
+        return;
     }
+
+    sort(begin, end - Diff{1});
 }
 
 } // namespace forfun::sorting
