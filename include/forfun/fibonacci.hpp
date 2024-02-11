fd766f2dd658cfa32255fdbc66c7d66a38913008
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-02-11 22:10:17 +0200

    Add nth Fibonacci number finder

---
+++ include/forfun/fibonacci.hpp
@@ -0,0 +1,42 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+/// Problem source:
+/// https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+#ifndef FORFUN_FIBONACCI_HPP_
+#define FORFUN_FIBONACCI_HPP_
+
+#include <type_traits>
+#include <utility>
+
+namespace forfun::fibonacci {
+
+namespace recursive {
+
+template <typename T>
+    requires std::integral<T>
+    and std::is_same_v<T, decltype(std::declval<T>() + std::declval<T>())>
+[[nodiscard]] constexpr inline T fib(T const n) noexcept
+{
+    if (n <= T{2})
+    {
+        if (n < T{1})
+        {
+            return T{0};
+        }
+
+        return T{1};
+    }
+
+    return fib(n - T{2}) + fib(n - T{1});
+}
+
+} // namespace recursive
+
+} // namespace forfun::fibonacci
+
+#endif // FORFUN_FIBONACCI_HPP_
