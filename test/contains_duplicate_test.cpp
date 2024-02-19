9f00305a13e647d7d1268ce9d593cd1c40c151b1
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-02-19 22:10:17 +0200

    test: Declare as static where workable

--- test/contains_duplicate_test.cpp
+++ test/contains_duplicate_test.cpp
@@ -16,25 +16,25 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Empty input")
     {
-        constexpr std::array<int, 0> const nums{};
-        constexpr bool const expected{false};
+        static constexpr std::array<int, 0> const nums{};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
-        REQUIRE(actual == expected);
+        STATIC_REQUIRE(actual == expected);
     }
 
     SECTION("Input length is one")
     {
-        constexpr std::array const nums{3};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{3};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -42,12 +42,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is two and contains duplicates")
     {
-        constexpr std::array const nums{3, 3};
-        constexpr bool const expected{true};
+        static constexpr std::array const nums{3, 3};
+        static constexpr bool const expected{true};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -55,12 +55,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is two and contains unique items #1")
     {
-        constexpr std::array const nums{2, 3};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{2, 3};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -68,12 +68,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is two and contains unique items #2")
     {
-        constexpr std::array const nums{3, 2};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{3, 2};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -81,12 +81,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is three and contains only duplicates")
     {
-        constexpr std::array const nums{19, 19, 19};
-        constexpr bool const expected{true};
+        static constexpr std::array const nums{19, 19, 19};
+        static constexpr bool const expected{true};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -94,12 +94,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is three and contains two duplicates")
     {
-        constexpr std::array const nums{19, 7, 19};
-        constexpr bool const expected{true};
+        static constexpr std::array const nums{19, 7, 19};
+        static constexpr bool const expected{true};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -107,12 +107,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is three and contains only unique items #1")
     {
-        constexpr std::array const nums{2, -5, 3};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{2, -5, 3};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -120,12 +120,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input length is three and contains only unique items #2")
     {
-        constexpr std::array const nums{3, 2, 1};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{3, 2, 1};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -133,12 +133,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input is of four sorted unique elements")
     {
-        constexpr std::array const nums{0.0f, 0.00000001f, 2.0f, 2.001f};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{0.0f, 0.00000001f, 2.0f, 2.001f};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -146,12 +146,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input is of four unsorted unique elements")
     {
-        constexpr std::array const nums{2.0f, 2.001f, 0.0f, 0.00000001f};
-        constexpr bool const expected{false};
+        static constexpr std::array const nums{2.0f, 2.001f, 0.0f, 0.00000001f};
+        static constexpr bool const expected{false};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
@@ -159,12 +159,12 @@ TEST_CASE("Contains Duplicate", "[contains_duplicate]")
 
     SECTION("Input is of four repeated elements")
     {
-        constexpr std::array const nums{11, 11, 11, 11};
-        constexpr bool const expected{true};
+        static constexpr std::array const nums{11, 11, 11, 11};
+        static constexpr bool const expected{true};
 
         CAPTURE(nums);
 
-        constexpr auto const actual{
+        static constexpr auto const actual{
             contains_duplicate(nums.cbegin(), nums.cend())};
 
         REQUIRE(actual == expected);
