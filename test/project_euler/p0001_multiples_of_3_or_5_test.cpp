362c5b35077076a6908d1ffe64b201c9b90e9f84
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-04-19 22:10:17 +0200

    test: Refactor tests

--- test/project_euler/p0001_multiples_of_3_or_5_test.cpp
+++ test/project_euler/p0001_multiples_of_3_or_5_test.cpp
@@ -4,6 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 
+#include <catch2/catch_message.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include "forfun/project_euler/p0001_multiples_of_3_or_5.hpp"
@@ -14,44 +15,47 @@ TEST_CASE("find_sum_mult_three_five", "[multiples_of_3_or_5]")
 
     SECTION("Static test 01")
     {
-        static constexpr int const s{find_sum_mult_three_five(1)};
-        STATIC_REQUIRE(s == 0);
+        static constexpr int const actual{find_sum_mult_three_five(1)};
+        STATIC_REQUIRE(actual == 0);
     }
 
     SECTION("Static test 02")
     {
-        static constexpr int const s{find_sum_mult_three_five(4)};
-        STATIC_REQUIRE(s == 3);
+        static constexpr int const actual{find_sum_mult_three_five(4)};
+        STATIC_REQUIRE(actual == 3);
     }
 
     SECTION("Static test 03")
     {
-        static constexpr int const s{find_sum_mult_three_five(6)};
-        STATIC_REQUIRE(s == 8);
+        static constexpr int const actual{find_sum_mult_three_five(6)};
+        STATIC_REQUIRE(actual == 8);
     }
 
     SECTION("Static test 04")
     {
-        static constexpr int const s{find_sum_mult_three_five(10)};
-        STATIC_REQUIRE(s == 23);
+        static constexpr int const actual{find_sum_mult_three_five(10)};
+        STATIC_REQUIRE(actual == 23);
     }
 
     SECTION("Static test 05")
     {
-        static constexpr int const s{find_sum_mult_three_five(11)};
-        STATIC_REQUIRE(s == 33);
+        static constexpr int const actual{find_sum_mult_three_five(11)};
+        STATIC_REQUIRE(actual == 33);
     }
 
     SECTION("Static test 06")
     {
-        static constexpr int const s{find_sum_mult_three_five(1000)};
-        STATIC_REQUIRE(s == 233168);
+        static constexpr int const actual{find_sum_mult_three_five(1000)};
+        STATIC_REQUIRE(actual == 233168);
     }
 
     SECTION("Runtime test")
     {
         int const volatile test_input{1000};
         int const actual{find_sum_mult_three_five(test_input)};
+
+        CAPTURE(test_input);
+
         REQUIRE(actual == 233168);
     }
 }
