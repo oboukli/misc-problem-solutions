66a13d88925a463502460c82c0fda2ad40ea4308
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-05-08 22:10:17 +0200

    Add Tower of Hanoi

---
+++ benchmark/tower_of_hanoi_benchmark.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <cstddef>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/tower_of_hanoi.hpp"
+
+TEST_CASE("tower_of_hanoi benchmarking", "[benchmark][tower_of_hanoi]")
+{
+    using namespace forfun::tower_of_hanoi;
+
+    using Rod = int;
+    using Monk = decltype([](Rod&, Rod&) noexcept -> void {});
+
+    ankerl::nanobench::Bench()
+
+        .title("Tower of Hanoi")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(recursive::toh<Rod, Monk, std::size_t>).c_str(),
+            []() {
+                static constexpr std::size_t const num_disks{4};
+                Rod src{};
+                Rod des{};
+                Rod aux{};
+
+                forfun::tower_of_hanoi::recursive::toh(
+                    src,
+                    des,
+                    aux,
+                    [](Rod&, Rod&) noexcept -> void {},
+                    num_disks);
+
+                ankerl::nanobench::doNotOptimizeAway(des);
+            })
+
+        ;
+}
