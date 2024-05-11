1901d119f9fc0cb695f73891ab1be7663f6dfdd8
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-05-11 22:10:17 +0200

    benchmark: Add majority_element

---
+++ benchmark/majority_element_benchmark.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <array>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/majority_element.hpp"
+
+TEST_CASE("majority_element benchmarking", "[benchmark][majority_element]")
+{
+    using namespace forfun::majority_element;
+    ankerl::nanobench::Bench()
+
+        .title("Boyer–Moore majority")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(majority_element<std::array<int, 64>>).c_str(),
+            []() {
+                static constexpr std::array const elements{
+                    5, 7, 7, 7, 7, 5, 5, 7, 5, 5, 5, 7, 7, 5, 5, 5,
+                    5, 7, 7, 7, 7, 5, 5, 7, 5, 5, 5, 7, 7, 5, 5, 5,
+                    5, 7, 7, 7, 7, 5, 5, 7, 5, 5, 5, 7, 7, 5, 5, 5,
+                    5, 7, 7, 7, 7, 5, 5, 7, 5, 5, 5, 7, 7, 5, 5, 5,
+                };
+                using ConstItr = decltype(elements)::const_iterator;
+
+                ConstItr r{majority_element(elements)};
+                ankerl::nanobench::doNotOptimizeAway(r);
+            })
+
+        ;
+}
