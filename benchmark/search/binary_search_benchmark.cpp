518b834ac66a0750722feae8a40ca4719a4139cb
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-05-16 22:10:17 +0200

    benchmark: Add benchmark for binary search

---
+++ benchmark/search/binary_search_benchmark.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <algorithm>
+#include <array>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/search/binary_search.hpp"
+
+TEST_CASE("Binary search benchmarking", "[benchmark][search][binary_search]")
+{
+    using namespace forfun::search::binary_search;
+    using Itr = std::array<int, 12>::const_iterator;
+
+    std::array const records{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    static_assert(records.size() == 12);
+
+    ankerl::nanobench::Bench()
+
+        .title("Binary search")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(std::find<Itr, int>).c_str(),
+            [&records]() {
+                Itr const r{std::find(records.cbegin(), records.cend(), 41)};
+                ankerl::nanobench::doNotOptimizeAway(r);
+            })
+
+        .run(
+            NAMEOF_RAW(iterative::find<Itr, int>).c_str(),
+            [&records]() {
+                Itr const r{
+                    iterative::find(records.cbegin(), records.cend(), 41)};
+                ankerl::nanobench::doNotOptimizeAway(r);
+            })
+
+        .run(
+            NAMEOF_RAW(recursive::find<Itr, int>).c_str(),
+            [&records]() {
+                Itr const r{
+                    recursive::find(records.cbegin(), records.cend(), 41)};
+                ankerl::nanobench::doNotOptimizeAway(r);
+            })
+
+        ;
+}
