c15d2236ab5d8343bcf930f7df39253cfc9cccd7
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-05-23 22:10:17 +0200

    test: Fix DFS test

--- test/graph/depth_first_search_test.cpp
+++ test/graph/depth_first_search_test.cpp
@@ -48,20 +48,67 @@ TEST_CASE("Depth-first search", "[graph][depth_first]")
         REQUIRE(adjacency_list.empty());
     }
 
-    SECTION("All graph vertices visited")
+    SECTION("All graph vertices visited, starting from leaf vertex")
     {
         VertexAdjacencyList<int> const adjacency_list{
             // clang-format off
-            {{1}, {2}, {3}, {4},},
-            {{2},},
-            {{3},},
-            {{4}, {5},},
-            {{5}, {6},},
-            {{6},},
+            {{1}, {2}, {3}, {4}},
+            {{2}, {1}},
+            {{3}, {1}},
+            {{4}, {1}, {5}},
+            {{5}, {4}, {6}},
+            {{6}, {5}},
             // clang-format on
         };
         VertexStateList<int> state_list{};
         constexpr vertex<int> const starting_vertex{1};
+
+        state_list.reserve(adjacency_list.size());
+        init_state_list(adjacency_list, state_list);
+
+        assert(adjacency_list.size() == 6);
+        assert(state_list.size() == 6);
+        assert(state_list.at(starting_vertex) == vertex_visit_state::unvisited);
+
+        CAPTURE(adjacency_list);
+        CAPTURE(state_list);
+        CAPTURE(starting_vertex);
+
+        depth_first_search(
+            adjacency_list,
+            state_list,
+            starting_vertex,
+            [](vertex<int>) noexcept {});
+
+        VertexStateList<int> const expected_state_list{
+            // clang-format off
+            {{1}, vertex_visit_state::visited},
+            {{2}, vertex_visit_state::visited},
+            {{3}, vertex_visit_state::visited},
+            {{4}, vertex_visit_state::visited},
+            {{5}, vertex_visit_state::visited},
+            {{6}, vertex_visit_state::visited},
+            // clang-format on
+        };
+
+        REQUIRE(state_list == expected_state_list);
+        REQUIRE(adjacency_list.size() == 6);
+    }
+
+    SECTION("All graph vertices visited, starting from non-leaf vertex")
+    {
+        VertexAdjacencyList<int> const adjacency_list{
+            // clang-format off
+            {{1}, {2}, {3}, {4}},
+            {{2}, {1}},
+            {{3}, {1}},
+            {{4}, {1}, {5}},
+            {{5}, {4}, {6}},
+            {{6}, {5}},
+            // clang-format on
+        };
+        VertexStateList<int> state_list{};
+        constexpr vertex<int> const starting_vertex{5};
 
         state_list.reserve(adjacency_list.size());
         init_state_list(adjacency_list, state_list);
