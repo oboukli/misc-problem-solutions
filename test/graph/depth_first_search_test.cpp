410926043728982c76a973d248540f75f39fd1a6
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-05-24 22:10:17 +0200

    test: Add more DFS test cases

--- test/graph/depth_first_search_test.cpp
+++ test/graph/depth_first_search_test.cpp
@@ -5,12 +5,24 @@
 // SPDX-License-Identifier: MIT
 
 #include <cassert>
+#include <cstddef>
 #include <vector>
+
+#define CATCH_CONFIG_ENABLE_PAIR_STRINGMAKER
+
 #include <catch2/catch_message.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_tostring.hpp>
 
 #include "forfun/graph/depth_first_search.hpp"
 #include "forfun/graph/vertex.hpp"
+
+CATCH_REGISTER_ENUM(
+    forfun::graph::vertex_visit_state,
+    forfun::graph::vertex_visit_state::unvisited,
+    forfun::graph::vertex_visit_state::visited,
+    //
+)
 
 TEST_CASE("Depth-first search", "[graph][depth_first]")
 {
@@ -140,5 +152,140 @@ TEST_CASE("Depth-first search", "[graph][depth_first]")
 
         REQUIRE(state_list == expected_state_list);
         REQUIRE(adjacency_list.size() == 6);
+    }
+
+    SECTION("All graph vertices (vertex<char>) visited")
+    {
+        VertexAdjacencyList<char> const adjacency_list{
+            // clang-format off
+            {{'a'}, {'b'}, {'c'}, {'d'}},
+            {{'b'}, {'a'}},
+            {{'c'}, {'a'}},
+            {{'d'}, {'a'}, {'e'}},
+            {{'e'}, {'d'}, {'f'}, {'g'}},
+            {{'f'}, {'e'}, {'h'}},
+            {{'g'}, {'e'}, {'h'}},
+            {{'h'}, {'f'}, {'g'}},
+            // clang-format on
+        };
+        VertexStateList<char> state_list{};
+        constexpr vertex<char> const starting_vertex{'c'};
+
+        state_list.reserve(adjacency_list.size());
+        init_state_list(adjacency_list, state_list);
+
+        assert(adjacency_list.size() == 8);
+        assert(state_list.size() == 8);
+        assert(state_list.at(starting_vertex) == vertex_visit_state::unvisited);
+
+        CAPTURE(adjacency_list);
+        CAPTURE(state_list);
+        CAPTURE(starting_vertex);
+
+        depth_first_search(
+            adjacency_list,
+            state_list,
+            starting_vertex,
+            [](vertex<char>) noexcept {});
+
+        VertexStateList<char> const expected_state_list{
+            // clang-format off
+            {{'a'}, vertex_visit_state::visited},
+            {{'b'}, vertex_visit_state::visited},
+            {{'c'}, vertex_visit_state::visited},
+            {{'d'}, vertex_visit_state::visited},
+            {{'e'}, vertex_visit_state::visited},
+            {{'f'}, vertex_visit_state::visited},
+            {{'g'}, vertex_visit_state::visited},
+            {{'h'}, vertex_visit_state::visited},
+            // clang-format on
+        };
+
+        REQUIRE(state_list == expected_state_list);
+        REQUIRE(adjacency_list.size() == 8);
+    }
+
+    SECTION("Visitor call count is the same as the vertex count")
+    {
+        VertexAdjacencyList<char> const adjacency_list{
+            // clang-format off
+            {{'a'}, {'b'}, {'c'}, {'d'}},
+            {{'b'}, {'a'}},
+            {{'c'}, {'a'}},
+            {{'d'}, {'a'}, {'e'}},
+            {{'e'}, {'d'}, {'f'}, {'g'}},
+            {{'f'}, {'e'}, {'h'}},
+            {{'g'}, {'e'}, {'h'}},
+            {{'h'}, {'f'}, {'g'}},
+            // clang-format on
+        };
+        VertexStateList<char> state_list{};
+        constexpr vertex<char> const starting_vertex{'h'};
+
+        state_list.reserve(adjacency_list.size());
+        init_state_list(adjacency_list, state_list);
+
+        assert(adjacency_list.size() == 8);
+        assert(state_list.size() == 8);
+        assert(state_list.at(starting_vertex) == vertex_visit_state::unvisited);
+
+        CAPTURE(adjacency_list);
+        CAPTURE(state_list);
+        CAPTURE(starting_vertex);
+
+        std::size_t call_count{};
+        depth_first_search(
+            adjacency_list,
+            state_list,
+            starting_vertex,
+            [&call_count](vertex<char>) noexcept { ++call_count; });
+
+        REQUIRE(call_count == 8);
+    }
+
+    SECTION("Visitor is called and is passed valid arguments to")
+    {
+        VertexAdjacencyList<char> const adjacency_list{
+            // clang-format off
+            {{'a'}, {'b'}, {'c'}, {'d'}},
+            {{'b'}, {'a'}},
+            {{'c'}, {'a'}},
+            {{'d'}, {'a'}, {'e'}},
+            {{'e'}, {'d'}, {'f'}, {'g'}},
+            {{'f'}, {'e'}, {'h'}},
+            {{'g'}, {'e'}, {'h'}},
+            {{'h'}, {'f'}, {'g'}},
+            // clang-format on
+        };
+        VertexStateList<char> state_list{};
+        constexpr vertex<char> const starting_vertex{'e'};
+
+        state_list.reserve(adjacency_list.size());
+        init_state_list(adjacency_list, state_list);
+
+        assert(adjacency_list.size() == 8);
+        assert(state_list.size() == 8);
+        assert(state_list.at(starting_vertex) == vertex_visit_state::unvisited);
+
+        CAPTURE(adjacency_list);
+        CAPTURE(state_list);
+        CAPTURE(starting_vertex);
+
+        std::vector<vertex<char>> recorded_path{};
+        recorded_path.reserve(8);
+        assert(recorded_path.empty());
+        depth_first_search(
+            adjacency_list,
+            state_list,
+            starting_vertex,
+            [&recorded_path](vertex<char> v) noexcept {
+                recorded_path.emplace_back(v);
+            });
+
+        REQUIRE(recorded_path.size() == 8);
+
+        std::vector<vertex<char>> const expected_recorded_path{
+            {{'e'}, {'d'}, {'a'}, {'b'}, {'c'}, {'f'}, {'h'}, {'g'}}};
+        REQUIRE(recorded_path == expected_recorded_path);
     }
 }
