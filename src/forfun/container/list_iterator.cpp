7e3eaf7b6ac4d6195565f4f9dfe0aa4c62523fae
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-08-24 22:50:17 +0200

    refactor: Correct method implementation files

--- src/forfun/container/list_iterator.cpp
+++ src/forfun/container/list_iterator.cpp
@@ -4,19 +4,4 @@
 
 // SPDX-License-Identifier: MIT
 
-#include "forfun/container/list_iterator.hpp"
-#include "forfun/container/list.hpp"
-
-namespace forfun::experimental::container {
-
-[[nodiscard]] auto list::begin() const noexcept -> iterator
-{
-    return list_iterator(head_);
-}
-
-[[nodiscard]] auto list::end() const noexcept -> iterator
-{
-    return list_iterator{nullptr};
-}
-
-} // namespace forfun::experimental::container
+#include "forfun/container/list_iterator.hpp" // IWYU pragma: keep
