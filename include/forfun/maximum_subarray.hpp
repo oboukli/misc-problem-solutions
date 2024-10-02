91abd52ad61d3f199ab47399f76b2f44bade379a
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-02 22:15:17 +0200

    feat: Add maximum subarray recursive strategy

--- include/forfun/maximum_subarray.hpp
+++ include/forfun/maximum_subarray.hpp
@@ -24,6 +24,48 @@ namespace brute_force {
 
 } // namespace brute_force
 
+namespace recursive {
+
+namespace detail {
+
+template <std::forward_iterator Iter, std::sentinel_for<Iter> Sentinel>
+    requires std::integral<std::iter_value_t<Iter>>
+[[nodiscard]] constexpr auto max_sum_internal(
+    Iter iter,
+    Sentinel const end,
+    PromotedValueType<std::iter_value_t<Iter>> sum
+) noexcept -> PromotedValueType<std::iter_value_t<Iter>>
+{
+    if (iter == end)
+    {
+        return sum;
+    }
+
+    PromotedValueType<std::iter_value_t<Iter>> const n{*iter};
+    sum = std::max(n, n + sum);
+
+    return std::max(sum, max_sum_internal(++iter, end, sum));
+}
+
+} // namespace detail
+
+template <std::forward_iterator Iter, std::sentinel_for<Iter> Sentinel>
+    requires std::integral<std::iter_value_t<Iter>>
+[[nodiscard]] constexpr auto max_sum(Iter iter, Sentinel const end) noexcept
+    -> PromotedValueType<std::iter_value_t<Iter>>
+{
+    using T = PromotedValueType<std::iter_value_t<Iter>>;
+
+    if (iter == end)
+    {
+        return T{0};
+    }
+
+    return detail::max_sum_internal(iter, end, std::min(T{0}, *iter));
+}
+
+} // namespace recursive
+
 namespace streaming {
 
 template <std::forward_iterator Iter, std::sentinel_for<Iter> Sentinel>
