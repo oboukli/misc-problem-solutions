51956f4d2ed3490c4307c1ea55e5498f09c56e17
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-02 22:20:17 +0200

    test: Add maximum subarray static tests

--- test/maximum_subarray_test.cpp
+++ test/maximum_subarray_test.cpp
@@ -4,6 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 
+#include <array>
 #include <vector>
 
 #include <catch2/catch_message.hpp>
@@ -11,6 +12,45 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "forfun/maximum_subarray.hpp"
+
+TEST_CASE("Maximum subarray constexpr tests", "[maximum_subarray]")
+{
+    SECTION("Static brute_force::max_sum(...)")
+    {
+        constexpr std::array const input{0};
+
+        STATIC_REQUIRE(
+            forfun::maximum_subarray::brute_force::max_sum(
+                input.cbegin(), input.cend()
+            )
+            == 0
+        );
+    }
+
+    SECTION("Static recursive::max_sum(...)")
+    {
+        constexpr std::array const input{0};
+
+        STATIC_REQUIRE(
+            forfun::maximum_subarray::recursive::max_sum(
+                input.cbegin(), input.cend()
+            )
+            == 0
+        );
+    }
+
+    SECTION("Static streaming::max_sum(...)")
+    {
+        constexpr std::array const input{0};
+
+        STATIC_REQUIRE(
+            forfun::maximum_subarray::streaming::max_sum(
+                input.cbegin(), input.cend()
+            )
+            == 0
+        );
+    }
+}
 
 TEMPLATE_TEST_CASE_SIG(
     "Maximum subarray",
