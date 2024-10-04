bda2c6de1c2a0d5223e2af55df03224a0485e7d8
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-04 22:30:17 +0200

    benchmark: Add two sum benchmark

---
+++ benchmark/two_sum_benchmark.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <array>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/two_sum.hpp"
+
+TEST_CASE("Two sum benchmarking", "[benchmark][two_sum]")
+{
+    using namespace forfun::two_sum;
+
+    std::array const nums{
+        2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,  37,  41,
+        43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97,  101,
+        103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+        173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239,
+        241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+    };
+
+    using ConstIter = decltype(nums)::const_iterator;
+
+    ankerl::nanobench::Bench()
+
+        .title("Two sum")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(two_sum<ConstIter, ConstIter>).c_str(),
+            [&nums]() noexcept {
+                auto const volatile r{two_sum(nums.cbegin(), nums.cend(), 681)};
+                ankerl::nanobench::doNotOptimizeAway(&r);
+            }
+        )
+
+        ;
+}
