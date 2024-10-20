8599e557368b8e17d97e76aeb4e2f42a8ce0793e
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-20 22:20:18 +0200

    benchmark: Refactor last stone weight

--- benchmark/last_stone_weight_benchmark.cpp
+++ benchmark/last_stone_weight_benchmark.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Last stone weight benchmarking", "[benchmark][last_stone_weight]")
         .run(
             NAMEOF_RAW(naive::last_stone_weight<Iter, Iter>).c_str(),
             []() noexcept {
-                std::vector<int> stones{
+                std::vector stones{
                     2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,
                     37,  41,  43,  47,  53,  59,  61,  67,  71,  73,  79,
                     83,  89,  97,  101, 103, 107, 109, 113, 127, 131, 137,
@@ -47,7 +47,7 @@ TEST_CASE("Last stone weight benchmarking", "[benchmark][last_stone_weight]")
         .run(
             NAMEOF_RAW(partially_sorted::last_stone_weight<Iter, Iter>).c_str(),
             []() noexcept {
-                std::vector<int> stones{
+                std::vector stones{
                     2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,
                     37,  41,  43,  47,  53,  59,  61,  67,  71,  73,  79,
                     83,  89,  97,  101, 103, 107, 109, 113, 127, 131, 137,
@@ -65,7 +65,7 @@ TEST_CASE("Last stone weight benchmarking", "[benchmark][last_stone_weight]")
         .run(
             NAMEOF_RAW(priority_queue_based::last_stone_weight).c_str(),
             []() noexcept {
-                std::vector<int> stones{
+                std::vector stones{
                     2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,
                     37,  41,  43,  47,  53,  59,  61,  67,  71,  73,  79,
                     83,  89,  97,  101, 103, 107, 109, 113, 127, 131, 137,
@@ -83,7 +83,7 @@ TEST_CASE("Last stone weight benchmarking", "[benchmark][last_stone_weight]")
         .run(
             NAMEOF_RAW(heapified::last_stone_weight<Iter, Iter>).c_str(),
             []() noexcept {
-                std::vector<int> stones{
+                std::vector stones{
                     2,   3,   5,   7,   11,  13,  17,  19,  23,  29,  31,
                     37,  41,  43,  47,  53,  59,  61,  67,  71,  73,  79,
                     83,  89,  97,  101, 103, 107, 109, 113, 127, 131, 137,
