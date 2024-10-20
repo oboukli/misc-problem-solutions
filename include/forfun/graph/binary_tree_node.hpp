d3c7c8fa408c15a4ef51a468c1c2e121f0d0e625
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-20 22:30:18 +0200

    feat: Add binary tree node

---
+++ include/forfun/graph/binary_tree_node.hpp
@@ -0,0 +1,46 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#ifndef FORFUN_GRAPH_BINARY_TREE_NODE_HPP_
+#define FORFUN_GRAPH_BINARY_TREE_NODE_HPP_
+
+namespace forfun::graph {
+
+class binary_tree_node final {
+public:
+    using value_type = int;
+
+    binary_tree_node() noexcept :
+        value_{}, left_node_{nullptr}, right_node_{nullptr}
+    {
+    }
+
+    explicit binary_tree_node(value_type value) noexcept :
+        value_{value}, left_node_{nullptr}, right_node_{nullptr}
+    {
+    }
+
+    binary_tree_node(
+        // NOLINTBEGIN(bugprone-easily-swappable-parameters)
+        value_type value,
+        binary_tree_node* left,
+        binary_tree_node* right
+        // NOLINTEND(bugprone-easily-swappable-parameters)
+    ) noexcept :
+        value_{value}, left_node_{left}, right_node_{right}
+    {
+    }
+
+    value_type value_;
+
+    binary_tree_node* left_node_;
+
+    binary_tree_node* right_node_;
+};
+
+} // namespace forfun::graph
+
+#endif // FORFUN_GRAPH_BINARY_TREE_NODE_HPP_
