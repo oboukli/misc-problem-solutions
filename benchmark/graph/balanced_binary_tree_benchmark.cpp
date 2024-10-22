f86369e50a2d424b838c21ec78e6f0bda391c7dd
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-22 22:25:18 +0200

    benchmark: Add balanced binary tree benchmark

---
+++ benchmark/graph/balanced_binary_tree_benchmark.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/graph/balanced_binary_tree.hpp"
+
+TEST_CASE(
+    "Balanced binary tree benchmarking", "[benchmark][balanced_binary_tree]"
+)
+{
+    using namespace forfun::graph::balanced_binary_tree;
+
+    // As seen in graph 4 in graph docs.
+    binary_tree_node h_r_r_l_{12};
+    binary_tree_node h_r_l_l{8};
+    binary_tree_node h_r_l_r{9};
+    binary_tree_node h_r_r_l{10, &h_r_r_l_, nullptr};
+    binary_tree_node h_r_r_r{11};
+    binary_tree_node h_l_l{4};
+    binary_tree_node h_l_r{5};
+    binary_tree_node h_r_l{6, &h_r_l_l, &h_r_l_r};
+    binary_tree_node h_r_r{7, &h_r_r_l, &h_r_r_r};
+    binary_tree_node h_l{2, &h_l_l, &h_l_r};
+    binary_tree_node h_r{3, &h_r_l, &h_r_r};
+    binary_tree_node const root{1, &h_l, &h_r};
+
+    ankerl::nanobench::Bench()
+
+        .title("Balanced binary tree")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(complicated::is_balanced).c_str(),
+            [&root]() noexcept {
+                auto const volatile r{complicated::is_balanced(&root)};
+                ankerl::nanobench::doNotOptimizeAway(&r);
+            }
+        )
+
+        .run(
+            NAMEOF_RAW(hacky::is_balanced).c_str(),
+            [&root]() noexcept {
+                auto const volatile r{hacky::is_balanced(&root)};
+                ankerl::nanobench::doNotOptimizeAway(&r);
+            }
+        )
+
+        .run(
+            NAMEOF_RAW(simple::is_balanced).c_str(),
+            [&root]() noexcept {
+                auto const volatile r{simple::is_balanced(&root)};
+                ankerl::nanobench::doNotOptimizeAway(&r);
+            }
+        )
+
+        ;
+}
