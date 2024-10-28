ca8f5764e6ebd9e88de9a2480a47caf33df53ff9
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-28 22:10:18 +0200

    refactor: Change interface

--- benchmark/best_time_to_buy_and_sell_stock_benchmark.cpp
+++ benchmark/best_time_to_buy_and_sell_stock_benchmark.cpp
@@ -20,6 +20,7 @@ TEST_CASE(
 )
 {
     using namespace forfun;
+    using Iter = std::vector<int>::const_iterator;
 
     std::vector const prices{
         2,   5,   11,  17,  23,  29,  41,  47,  53,  59,  71,
@@ -33,39 +34,37 @@ TEST_CASE(
         .relative(true)
 
         .run(
-            NAMEOF_RAW(best_time_to_buy_and_sell_stock::calc_max_profit)
+            NAMEOF_RAW(best_time_to_buy_and_sell_stock::calc_max_profit<Iter, Iter>)
                 .c_str(),
             [&prices]() noexcept {
                 auto const volatile r{
-                    best_time_to_buy_and_sell_stock::calc_max_profit(prices)
+                    best_time_to_buy_and_sell_stock::calc_max_profit(
+                        prices.cbegin(), prices.cend()
+                    )
                 };
                 ankerl::nanobench::doNotOptimizeAway(&r);
             }
         )
 
         .run(
-            NAMEOF_RAW(
-                best_time_to_buy_and_sell_stock::optimized_l1::calc_max_profit
-            )
+            NAMEOF_RAW(best_time_to_buy_and_sell_stock::optimized_l1::calc_max_profit<Iter, Iter>)
                 .c_str(),
             [&prices]() noexcept {
                 auto const volatile r{
                     best_time_to_buy_and_sell_stock::optimized_l1::
-                        calc_max_profit(prices)
+                        calc_max_profit(prices.cbegin(), prices.cend())
                 };
                 ankerl::nanobench::doNotOptimizeAway(&r);
             }
         )
 
         .run(
-            NAMEOF_RAW(
-                best_time_to_buy_and_sell_stock::optimized_l2::calc_max_profit
-            )
+            NAMEOF_RAW(best_time_to_buy_and_sell_stock::optimized_l2::calc_max_profit<Iter, Iter>)
                 .c_str(),
             [&prices]() noexcept {
                 auto const volatile r{
                     best_time_to_buy_and_sell_stock::optimized_l2::
-                        calc_max_profit(prices)
+                        calc_max_profit(prices.cbegin(), prices.cend())
                 };
                 ankerl::nanobench::doNotOptimizeAway(&r);
             }
--- test/best_time_to_buy_and_sell_stock_test.cpp
+++ test/best_time_to_buy_and_sell_stock_test.cpp
@@ -4,7 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 
-#include <array>
+#include <vector>
 
 #include <catch2/catch_message.hpp>
 #include <catch2/catch_template_test_macros.hpp>
@@ -16,77 +16,83 @@ TEMPLATE_TEST_CASE_SIG(
     "Best time to buy and sell stock",
     "[best_time_to_buy_and_sell_stock]",
     (auto calc_max_profit, calc_max_profit),
-    forfun::best_time_to_buy_and_sell_stock::calc_max_profit,
-    forfun::best_time_to_buy_and_sell_stock::optimized_l1::calc_max_profit,
-    forfun::best_time_to_buy_and_sell_stock::optimized_l2::calc_max_profit
+    (forfun::best_time_to_buy_and_sell_stock::calc_max_profit<
+        std::vector<int>::const_iterator,
+        std::vector<int>::const_iterator>),
+    (forfun::best_time_to_buy_and_sell_stock::optimized_l1::calc_max_profit<
+        std::vector<int>::const_iterator,
+        std::vector<int>::const_iterator>),
+    (forfun::best_time_to_buy_and_sell_stock::optimized_l2::calc_max_profit<
+        std::vector<int>::const_iterator,
+        std::vector<int>::const_iterator>)
 )
 {
     SECTION("Empty price container")
     {
-        std::array<int, 0U> const prices{};
+        std::vector<int> const prices{};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("One price")
     {
-        std::array const prices{19};
+        std::vector const prices{19};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("Two prices where profit is possible")
     {
-        std::array const prices{7, 19};
+        std::vector const prices{7, 19};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 12);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 12);
     }
 
     SECTION("Two prices where profit is impossible")
     {
-        std::array const prices{19, 7};
+        std::vector const prices{19, 7};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("Three prices where profit is possible - case 1")
     {
-        std::array const prices{5, 7, 19};
+        std::vector const prices{5, 7, 19};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 14);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 14);
     }
 
     SECTION("Three prices where profit is possible - case 2")
     {
-        std::array const prices{7, 5, 19};
+        std::vector const prices{7, 5, 19};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 14);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 14);
     }
 
     SECTION("Three prices where profit is impossible")
     {
-        std::array const prices{19, 7, 3};
+        std::vector const prices{19, 7, 3};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("Thirty-two prices where profit is possible (benchmark case)")
     {
-        std::array const prices{
+        std::vector const prices{
             2,   5,   11,  17,  23,  29,  41,  47,  53,  59,  71,
             83,  89,  101, 107, 113, 131, 137, 149, 167, 173, 179,
             191, 197, 227, 233, 239, 251, 257, 263, 269, 271,
@@ -94,12 +100,12 @@ TEMPLATE_TEST_CASE_SIG(
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 269);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 269);
     }
 
     SECTION("Thirty-two prices where profit is impossible")
     {
-        std::array const prices{
+        std::vector const prices{
             271, 269, 263, 257, 251, 239, 233, 227, 197, 191, 179,
             173, 167, 149, 137, 131, 113, 107, 101, 89,  83,  71,
             59,  53,  47,  41,  29,  23,  17,  11,  5,   2,
@@ -107,69 +113,69 @@ TEMPLATE_TEST_CASE_SIG(
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("LeetCode test case 1")
     {
-        std::array const prices{7, 1, 5, 3, 6, 4};
+        std::vector const prices{7, 1, 5, 3, 6, 4};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 5);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 5);
     }
 
     SECTION("LeetCode test case 2")
     {
-        std::array const prices{7, 6, 4, 3, 1};
+        std::vector const prices{7, 6, 4, 3, 1};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("NeetCode test case 1")
     {
-        std::array const prices{10, 1, 5, 6, 7, 1};
+        std::vector const prices{10, 1, 5, 6, 7, 1};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 6);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 6);
     }
 
     SECTION("NeetCode test case 2")
     {
-        std::array const prices{10, 8, 7, 5, 2};
+        std::vector const prices{10, 8, 7, 5, 2};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("GeeksforGeeks test case 1")
     {
-        std::array const prices{7, 10, 1, 3, 6, 9, 2};
+        std::vector const prices{7, 10, 1, 3, 6, 9, 2};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 8);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 8);
     }
 
     SECTION("GeeksforGeeks test case 2")
     {
-        std::array const prices{7, 6, 4, 3, 1};
+        std::vector const prices{7, 6, 4, 3, 1};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 0);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 0);
     }
 
     SECTION("GeeksforGeeks test case 3")
     {
-        std::array const prices{1, 3, 6, 9, 11};
+        std::vector const prices{1, 3, 6, 9, 11};
 
         CAPTURE(prices);
 
-        REQUIRE(calc_max_profit(prices) == 10);
+        REQUIRE(calc_max_profit(prices.cbegin(), prices.cend()) == 10);
     }
 }
