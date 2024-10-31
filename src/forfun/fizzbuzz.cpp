5ca22c8303fdab6571868bfede7b4cb6ae90129e
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-10-31 22:40:18 +0200

    refactor: Utilize `string_view` in FizzBuzz

--- src/forfun/fizzbuzz.cpp
+++ src/forfun/fizzbuzz.cpp
@@ -6,27 +6,32 @@
 
 #include "forfun/fizzbuzz.hpp"
 
-#include <ios>
 #include <ostream>
+#include <string_view>
 
 namespace forfun::fizzbuzz {
 
 auto fizzbuzz(int const n, std::ostream& stream) -> void
 {
+    static constexpr int const fizz_divider{3};
+    static constexpr int const buzz_divider{5};
+    static constexpr std::string_view const fizz{"Fizz"};
+    static constexpr std::string_view const buzz{"Buzz"};
+
     for (int i{1}; i <= n; ++i)
     {
         bool is_numeric{true};
 
-        if ((i % 3) == 0)
+        if ((i % fizz_divider) == 0)
         {
-            stream.write("Fizz", std::streamsize{4});
+            stream.write(fizz.data(), fizz.size());
 
             is_numeric = false;
         }
 
-        if ((i % 5) == 0)
+        if ((i % buzz_divider) == 0)
         {
-            stream.write("Buzz", std::streamsize{4});
+            stream.write(buzz.data(), buzz.size());
         }
         else if (is_numeric)
         {
