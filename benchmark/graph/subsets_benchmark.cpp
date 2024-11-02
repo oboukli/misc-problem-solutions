594518f18eda2d10b43facd90808a0ffa7ed36e3
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2024-11-02 22:15:18 +0200

    benchmark: Add Subsets

---
+++ benchmark/graph/subsets_benchmark.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <nanobench.h>
+
+#include <nameof.hpp>
+
+#include "forfun/graph/subsets.hpp"
+
+TEST_CASE("Subsets benchmarking", "[benchmark][subsets]")
+{
+    using namespace forfun::subsets;
+
+    std::vector<int> const elements{23, 29, 31, 37};
+
+    ankerl::nanobench::Bench()
+
+        .title("Subsets")
+        .relative(true)
+
+        .run(
+            NAMEOF_RAW(forfun::subsets::explode_subsets).c_str(),
+            [&elements]() noexcept {
+                auto const volatile r{
+                    forfun::subsets::explode_subsets(elements)
+                };
+                ankerl::nanobench::doNotOptimizeAway(&r);
+            }
+        )
+
+        ;
+}
