398c1bad81311896c94307a14587f22e2c3cee07
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-02-15 22:10:18 +0200

    test: Revisit Find Fibonacci number tests

--- test/fibonacci_static_test.cpp
+++ test/fibonacci_static_test.cpp
@@ -4,7 +4,6 @@
 
 // SPDX-License-Identifier: MIT
 
-#include <catch2/catch_message.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
@@ -22,46 +21,36 @@ TEMPLATE_TEST_CASE_SIG(
 {
     SECTION("Negative value case (n = -1)")
     {
-        auto const volatile n{-1};
+        static constexpr int const n{-1};
 
-        CAPTURE(n);
-
-        REQUIRE(fib(n) == 0);
+        STATIC_REQUIRE(fib(n) == 0);
     }
 
     SECTION("First (zeroth) Fibonacci number (n = 0) is zero")
     {
-        auto const volatile n{0};
+        static constexpr int const n{0};
 
-        CAPTURE(n);
-
-        REQUIRE(fib(n) == 0);
+        STATIC_REQUIRE(fib(n) == 0);
     }
 
     SECTION("Second Fibonacci number (n = 1) is one")
     {
-        auto const volatile n{1};
+        static constexpr int const n{1};
 
-        CAPTURE(n);
-
-        REQUIRE(fib(n) == 1);
+        STATIC_REQUIRE(fib(n) == 1);
     }
 
     SECTION("Third Fibonacci number (n = 2) is one")
     {
-        auto const volatile n{2};
+        static constexpr int const n{2};
 
-        CAPTURE(n);
-
-        REQUIRE(fib(n) == 1);
+        STATIC_REQUIRE(fib(n) == 1);
     }
 
     SECTION("Eighth Fibonacci number is (n = 7) thirteen")
     {
-        auto const volatile n{7};
+        static constexpr int const n{7};
 
-        CAPTURE(n);
-
-        REQUIRE(fib(n) == 13);
+        STATIC_REQUIRE(fib(n) == 13);
     }
 }
