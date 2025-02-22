f1e0124614e2c0b14f90dbf6fceadc16e80ba622
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-02-22 22:10:18 +0200

    refactor: Revisit sorting library

--- include/forfun/sorting/quicksort.hpp
+++ include/forfun/sorting/quicksort.hpp
@@ -11,53 +11,64 @@
 #define FORFUN_SORTING_QUICKSORT_HPP_
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
 
 namespace forfun::sorting {
 
 namespace detail {
 
-template <std::sortable IterA, std::bidirectional_iterator IterB>
+template <typename IterA, typename IterB>
+    requires std::sortable<IterA>
+    and std::bidirectional_iterator<IterA>
+    and std::bidirectional_iterator<IterB>
 [[nodiscard]] constexpr auto partition(IterA const first, IterB last) noexcept
     -> IterA
 {
+    using std::iter_swap;
+    using std::less;
+
     auto it_i{first};
 
+    auto const pivot{*first};
+    for (auto it_j{--last}; it_j != it_i;)
     {
-        auto const pivot{*first};
-        for (auto it_j{--last}; it_j != it_i;)
+        if (less{}(*it_j, pivot))
         {
-            if (*it_j < pivot)
-            {
-                std::iter_swap(++it_i, it_j);
-            }
-            else
-            {
-                --it_j;
-            }
+            iter_swap(++it_i, it_j);
+        }
+        else
+        {
+            --it_j;
         }
     }
 
-    std::iter_swap(first, it_i);
+    iter_swap(first, it_i);
 
     return it_i;
 }
 
 } // namespace detail
 
-template <std::sortable IterA, std::bidirectional_iterator IterB>
+template <typename IterA, typename IterB>
+    requires std::sortable<IterA>
+    and std::bidirectional_iterator<IterA>
+    and std::bidirectional_iterator<IterB>
 constexpr auto quicksort(IterA const first, IterB const last) noexcept -> void
 {
+    using std::distance;
+    using std::less;
+
     using DiffType = std::iter_difference_t<IterA>;
 
-    if (std::distance(first, last) < DiffType{2})
+    if (less{}(distance(first, last), DiffType{2}))
     {
         return;
     }
 
-    auto p{detail::partition(first, last)};
-    quicksort(first, p);
-    quicksort(++p, last);
+    auto partition{detail::partition(first, last)};
+    quicksort(first, partition);
+    quicksort(++partition, last);
 }
 
 } // namespace forfun::sorting
