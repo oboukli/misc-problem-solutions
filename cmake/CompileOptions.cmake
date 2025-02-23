bac0fb24df9e37e939e4ec774f17b72800f2ec4d
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-02-23 22:15:18 +0200

    build: Make compile options private

---
+++ cmake/CompileOptions.cmake
@@ -0,0 +1,27 @@
+# Copyright (c) Omar Boukli-Hacene. All rights reserved.
+# Distributed under an MIT-style license that can be
+# found in the LICENSE file.
+
+# SPDX-License-Identifier: MIT
+
+function(forfun_common_target_compile_options target)
+  separate_arguments(
+    FORFUN_COMMON_DEFINITIONS UNIX_COMMAND "${FORFUN_COMMON_DEFINITIONS}"
+  )
+  separate_arguments(
+    FORFUN_COMMON_CFLAGS UNIX_COMMAND "${FORFUN_COMMON_CFLAGS}"
+  )
+  separate_arguments(
+    FORFUN_COMMON_CXXFLAGS UNIX_COMMAND "${FORFUN_COMMON_CXXFLAGS}"
+  )
+
+  target_compile_definitions(
+    ${target}
+    PRIVATE ${FORFUN_COMMON_DEFINITIONS}
+  )
+  target_compile_options(
+    ${target}
+    PRIVATE $<$<COMPILE_LANGUAGE:C>:${FORFUN_COMMON_CFLAGS}>
+    PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${FORFUN_COMMON_CXXFLAGS}>
+  )
+endfunction()
