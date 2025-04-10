38f5026549f8ebc59adb252472c1e950b58e7804
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-04-10 20:56:26 +0200

    feat: Add shim memory allocator library

---
+++ src/forfun_c/shim/allocator.c
@@ -0,0 +1,110 @@
+/*
+Copyright (c) Omar Boukli-Hacene. All rights reserved.
+Distributed under an MIT-style license that can be
+found in the LICENSE file.
+*/
+
+/* SPDX-License-Identifier: MIT */
+
+#include "forfun_c/shim/allocator.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "forfun_c/mem/mem.h"
+
+typedef struct forfun_shim_context {
+    size_t shim_malloc_call_count;
+
+    size_t shim_free_call_count;
+
+    size_t shim_malloc_fail_after;
+} forfun_shim_context;
+
+static forfun_mem g_unshimmed_mem_ = {NULL, NULL};
+static forfun_shim_context g_shim_context_
+    = {0, 0, (size_t)-1 /* SIZE_MAX in C99 */};
+static int g_shim_is_enabled_ = 0;
+
+int forfun_shim_enable(void)
+{
+    forfun_mem mem;
+
+    if (g_shim_is_enabled_ == 1)
+    {
+        /* Error: shim is already initialized. */
+        return 1;
+    }
+
+    mem.ff_malloc = forfun_shim_malloc;
+    mem.ff_free = forfun_shim_free;
+
+    g_unshimmed_mem_ = forfun_mem_get();
+
+    forfun_mem_set(mem);
+
+    g_shim_is_enabled_ = 1;
+
+    return 0;
+}
+
+void forfun_shim_disable(void)
+{
+    forfun_mem_set(g_unshimmed_mem_);
+
+    g_shim_is_enabled_ = 0;
+}
+
+int forfun_shim_is_enabled(void)
+{
+    return g_shim_is_enabled_;
+}
+
+void forfun_shim_reset(void)
+{
+    g_shim_context_.shim_malloc_call_count = 0;
+    g_shim_context_.shim_free_call_count = 0;
+    g_shim_context_.shim_malloc_fail_after = (size_t)-1; /* SIZE_MAX in C99 */
+}
+
+size_t forfun_shim_get_malloc_call_count(void)
+{
+    return g_shim_context_.shim_malloc_call_count;
+}
+
+size_t forfun_shim_get_free_call_count(void)
+{
+    return g_shim_context_.shim_free_call_count;
+}
+
+size_t forfun_shim_get_malloc_fail_after(void)
+{
+    return g_shim_context_.shim_malloc_fail_after;
+}
+
+void forfun_shim_set_malloc_fail_after(size_t const fail_after)
+{
+    g_shim_context_.shim_malloc_fail_after = fail_after;
+}
+
+void* forfun_shim_malloc(size_t const size)
+{
+    ++g_shim_context_.shim_malloc_call_count;
+
+    if ((g_shim_context_.shim_malloc_fail_after
+         != (size_t)-1 /* SIZE_MAX in C99 */)
+        && (g_shim_context_.shim_malloc_call_count
+            > g_shim_context_.shim_malloc_fail_after))
+    {
+        return NULL;
+    }
+
+    return malloc(size);
+}
+
+void forfun_shim_free(void* const ptr)
+{
+    ++g_shim_context_.shim_free_call_count;
+
+    free(ptr);
+}
---
+++ test/shim/allocator_test.cpp
@@ -0,0 +1,411 @@
+// Copyright (c) Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+#include <cstddef>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "forfun_c/mem/mem.h"
+#include "forfun_c/shim/allocator.h"
+
+TEST_CASE("Shim memory allocator", "[shim_allocator]")
+{
+    SECTION("Confirm assumptions about mem state before enabling shim")
+    {
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        ::forfun_mem const mem_current_state{::forfun_mem_get()};
+
+        REQUIRE_FALSE(mem_current_state.ff_malloc == &::forfun_shim_malloc);
+        REQUIRE_FALSE(mem_current_state.ff_free == &::forfun_shim_free);
+    }
+
+    SECTION("Confirm state after enabling shim")
+    {
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        ::forfun_mem const mem_current_state{::forfun_mem_get()};
+
+        REQUIRE_FALSE(mem_current_state.ff_malloc == &::forfun_shim_malloc);
+        REQUIRE_FALSE(mem_current_state.ff_free == &::forfun_shim_free);
+
+        REQUIRE(::forfun_shim_enable() == 0);
+
+        ::forfun_mem const mem_state_after_shim_enable{::forfun_mem_get()};
+
+        REQUIRE(mem_state_after_shim_enable.ff_malloc == &::forfun_shim_malloc);
+        REQUIRE(mem_state_after_shim_enable.ff_free == &::forfun_shim_free);
+
+        REQUIRE(::forfun_shim_is_enabled() == 1);
+
+        ::forfun_shim_disable();
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+    }
+
+    SECTION("Confirm global state after disabling shim")
+    {
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        REQUIRE(::forfun_shim_enable() == 0);
+
+        ::forfun_mem const mem_current_state{::forfun_mem_get()};
+
+        REQUIRE(mem_current_state.ff_malloc == &::forfun_shim_malloc);
+        REQUIRE(mem_current_state.ff_free == &::forfun_shim_free);
+
+        ::forfun_shim_disable();
+
+        ::forfun_mem const mem_state_after_shim_disable{::forfun_mem_get()};
+
+        REQUIRE_FALSE(
+            mem_state_after_shim_disable.ff_malloc == &::forfun_shim_malloc
+        );
+        REQUIRE_FALSE(
+            mem_state_after_shim_disable.ff_free == &::forfun_shim_free
+        );
+
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+    }
+
+    SECTION("Error on enabling already enabled shim")
+    {
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        REQUIRE(::forfun_shim_enable() == 0);
+        REQUIRE(::forfun_shim_is_enabled() == 1);
+
+        REQUIRE(::forfun_shim_enable() == 1);
+        REQUIRE(::forfun_shim_is_enabled() == 1);
+
+        ::forfun_shim_disable();
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        REQUIRE(::forfun_shim_enable() == 0);
+        REQUIRE(::forfun_shim_is_enabled() == 1);
+
+        ::forfun_shim_disable();
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+    }
+
+    SECTION("Confirm shim inital state")
+    {
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Reset shim state")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Mutate then reset shim state")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_set_malloc_fail_after(7919);
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 7919);
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Happy journey of shim malloc and shim free invocations")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        {
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+
+            REQUIRE_FALSE(ptr.get() == nullptr);
+        }
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 1);
+        REQUIRE(::forfun_shim_get_free_call_count() == 1);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Shim simulate error on first invocation")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_set_malloc_fail_after(0);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 0);
+
+        {
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+
+            REQUIRE(ptr.get() == nullptr);
+        }
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 1);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 0);
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Shim simulate error on fifth invocation")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_set_malloc_fail_after(4);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 4);
+
+        {
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr1{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr2{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr3{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr4{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr5{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+
+            REQUIRE_FALSE(ptr1.get() == nullptr);
+            REQUIRE_FALSE(ptr2.get() == nullptr);
+            REQUIRE_FALSE(ptr3.get() == nullptr);
+            REQUIRE_FALSE(ptr4.get() == nullptr);
+            REQUIRE(ptr5.get() == nullptr);
+        }
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 5);
+        REQUIRE(::forfun_shim_get_free_call_count() == 4);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 4);
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Shim simulate error on sixth invocation")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_set_malloc_fail_after(5);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 5);
+
+        {
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr1{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr2{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr3{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr4{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr5{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr6{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+
+            REQUIRE_FALSE(ptr1.get() == nullptr);
+            REQUIRE_FALSE(ptr2.get() == nullptr);
+            REQUIRE_FALSE(ptr3.get() == nullptr);
+            REQUIRE_FALSE(ptr4.get() == nullptr);
+            REQUIRE_FALSE(ptr5.get() == nullptr);
+            REQUIRE(ptr6.get() == nullptr);
+        }
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 6);
+        REQUIRE(::forfun_shim_get_free_call_count() == 5);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 5);
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Shim simulate errors after seventh invocation")
+    {
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+
+        ::forfun_shim_set_malloc_fail_after(7);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 7);
+
+        {
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr1{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr2{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr3{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr4{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr5{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr6{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr7{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr8{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+            std::unique_ptr<void, decltype(&::forfun_shim_free)> const ptr9{
+                ::forfun_shim_malloc(sizeof(int)), &::forfun_shim_free
+            };
+
+            REQUIRE_FALSE(ptr1.get() == nullptr);
+            REQUIRE_FALSE(ptr2.get() == nullptr);
+            REQUIRE_FALSE(ptr3.get() == nullptr);
+            REQUIRE_FALSE(ptr4.get() == nullptr);
+            REQUIRE_FALSE(ptr5.get() == nullptr);
+            REQUIRE_FALSE(ptr6.get() == nullptr);
+            REQUIRE_FALSE(ptr7.get() == nullptr);
+            REQUIRE(ptr8.get() == nullptr);
+            REQUIRE(ptr9.get() == nullptr);
+        }
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 9);
+        REQUIRE(::forfun_shim_get_free_call_count() == 7);
+        REQUIRE(::forfun_shim_get_malloc_fail_after() == 7);
+
+        ::forfun_shim_reset();
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    SECTION("Invoking shim free with nullptr")
+    {
+        ::forfun_shim_reset();
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+
+        ::forfun_shim_free(nullptr);
+
+        REQUIRE(::forfun_shim_get_free_call_count() == 1);
+
+        ::forfun_shim_reset();
+    }
+
+    SECTION("Assert clean state after at test case end")
+    {
+        REQUIRE(::forfun_shim_is_enabled() == 0);
+
+        ::forfun_mem const mem_current_state{::forfun_mem_get()};
+
+        REQUIRE(mem_current_state.ff_malloc == &std::malloc);
+        REQUIRE(mem_current_state.ff_free == &std::free);
+
+        REQUIRE(::forfun_shim_get_malloc_call_count() == 0);
+        REQUIRE(::forfun_shim_get_free_call_count() == 0);
+        REQUIRE(
+            ::forfun_shim_get_malloc_fail_after()
+            == std::numeric_limits<std::size_t>::max()
+        );
+    }
+
+    // Do not mutate any (global) state beyond the last section.
+}
