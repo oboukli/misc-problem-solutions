cb456d3c6e27c73dbcf6e701b677a80eb9808fa1
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-04-10 21:57:04 +0200

    test: Test mem global state variable

--- test/mem/mem_test.cpp
+++ test/mem/mem_test.cpp
@@ -37,6 +37,14 @@ TEST_CASE("Memory allocator manager", "[mem]")
     SECTION("Confirm default global state")
     {
         ::forfun_mem_reset_to_default();
+
+        REQUIRE(::g_forfun_mem.ff_malloc == &std::malloc);
+        REQUIRE(::g_forfun_mem.ff_free == &std::free);
+    }
+
+    SECTION("Confirm default global state by getter")
+    {
+        ::forfun_mem_reset_to_default();
         ::forfun_mem const mem_current_state{::forfun_mem_get()};
 
         REQUIRE(mem_current_state.ff_malloc == &std::malloc);
@@ -48,22 +56,31 @@ TEST_CASE("Memory allocator manager", "[mem]")
         ::forfun_mem_reset_to_default();
         ::forfun_mem const mem_current_state{::forfun_mem_get()};
 
-        ::forfun_mem const mem{
+        ::forfun_mem const mem_dummy{
             .ff_malloc = &dummy_malloc,
             .ff_free = &dummy_free,
         };
 
-        REQUIRE_FALSE(mem.ff_malloc == mem_current_state.ff_malloc);
-        REQUIRE_FALSE(mem.ff_free == mem_current_state.ff_free);
+        REQUIRE_FALSE(mem_dummy.ff_malloc == ::g_forfun_mem.ff_malloc);
+        REQUIRE_FALSE(mem_dummy.ff_free == ::g_forfun_mem.ff_free);
 
-        ::forfun_mem_set(mem);
+        REQUIRE_FALSE(mem_dummy.ff_malloc == mem_current_state.ff_malloc);
+        REQUIRE_FALSE(mem_dummy.ff_free == mem_current_state.ff_free);
+
+        ::forfun_mem_set(mem_dummy);
+
+        REQUIRE(::g_forfun_mem.ff_malloc == &dummy_malloc);
+        REQUIRE(::g_forfun_mem.ff_free == &dummy_free);
+
         ::forfun_mem const mem_state_after_set{::forfun_mem_get()};
-
         REQUIRE(mem_state_after_set.ff_malloc == &dummy_malloc);
         REQUIRE(mem_state_after_set.ff_free == &dummy_free);
 
         ::forfun_mem_reset_to_default();
         ::forfun_mem const mem_reset_state{::forfun_mem_get()};
+
+        REQUIRE(::g_forfun_mem.ff_malloc == &std::malloc);
+        REQUIRE(::g_forfun_mem.ff_free == &std::free);
 
         REQUIRE(mem_reset_state.ff_malloc == &std::malloc);
         REQUIRE(mem_reset_state.ff_free == &std::free);
