14760f7e8114cd81f7fb454740326d594824819d
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-07-05 15:43:18 +0200

    test Revisit static test

--- test/maximum_subarray_statictest.cpp
+++ test/maximum_subarray_statictest.cpp
@@ -6,45 +6,68 @@
 
 #include <array>
 
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include "forfun/maximum_subarray.hpp"
 
-TEST_CASE("Maximum subarray constexpr tests", "[maximum_subarray]")
+TEMPLATE_TEST_CASE_SIG(
+    "Maximum subarray constexpr tests (empty array case)",
+    "[maximum_subarray]",
+    (auto max_sum, max_sum),
+    (forfun::maximum_subarray::brute_force::max_sum<
+        std::array<int, 0>::const_iterator,
+        std::array<int, 0>::const_iterator>),
+    (forfun::maximum_subarray::recursive::max_sum<
+        std::array<int, 0>::const_iterator,
+        std::array<int, 0>::const_iterator>),
+    (forfun::maximum_subarray::streaming::max_sum<
+        std::array<int, 0>::const_iterator,
+        std::array<int, 0>::const_iterator>)
+)
 {
-    SECTION("Static brute_force::max_sum(...)")
-    {
-        constexpr std::array const input{0};
+    static constexpr std::array<int, 0> const input{};
 
-        STATIC_REQUIRE(
-            forfun::maximum_subarray::brute_force::max_sum(
-                input.cbegin(), input.cend()
-            )
-            == 0
-        );
-    }
+    STATIC_REQUIRE(max_sum(input.cbegin(), input.cend()) == 0);
+}
 
-    SECTION("Static recursive::max_sum(...)")
-    {
-        constexpr std::array const input{0};
+TEMPLATE_TEST_CASE_SIG(
+    "Maximum subarray constexpr tests (one integer case)",
+    "[maximum_subarray]",
+    (auto max_sum, max_sum),
+    (forfun::maximum_subarray::brute_force::max_sum<
+        std::array<int, 1>::const_iterator,
+        std::array<int, 1>::const_iterator>),
+    (forfun::maximum_subarray::recursive::max_sum<
+        std::array<int, 1>::const_iterator,
+        std::array<int, 1>::const_iterator>),
+    (forfun::maximum_subarray::streaming::max_sum<
+        std::array<int, 1>::const_iterator,
+        std::array<int, 1>::const_iterator>)
+)
+{
+    static constexpr std::array const input{17};
+    static_assert(input.size() == 1UZ);
 
-        STATIC_REQUIRE(
-            forfun::maximum_subarray::recursive::max_sum(
-                input.cbegin(), input.cend()
-            )
-            == 0
-        );
-    }
+    STATIC_REQUIRE(max_sum(input.cbegin(), input.cend()) == 17);
+}
 
-    SECTION("Static streaming::max_sum(...)")
-    {
-        constexpr std::array const input{0};
+TEMPLATE_TEST_CASE_SIG(
+    "Maximum subarray constexpr tests (two integers case)",
+    "[maximum_subarray]",
+    (auto max_sum, max_sum),
+    (forfun::maximum_subarray::brute_force::max_sum<
+        std::array<int, 2>::const_iterator,
+        std::array<int, 2>::const_iterator>),
+    (forfun::maximum_subarray::recursive::max_sum<
+        std::array<int, 2>::const_iterator,
+        std::array<int, 2>::const_iterator>),
+    (forfun::maximum_subarray::streaming::max_sum<
+        std::array<int, 2>::const_iterator,
+        std::array<int, 2>::const_iterator>)
+)
+{
+    static constexpr std::array const input{61, 67};
 
-        STATIC_REQUIRE(
-            forfun::maximum_subarray::streaming::max_sum(
-                input.cbegin(), input.cend()
-            )
-            == 0
-        );
-    }
+    STATIC_REQUIRE(max_sum(input.cbegin(), input.cend()) == 128);
 }
