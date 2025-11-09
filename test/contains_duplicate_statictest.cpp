d5209af0fdac9da427b524b6832219ed3a6e8738
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-11-09 22:50:18 +0200

    test: Revisit static test

--- test/contains_duplicate_statictest.cpp
+++ test/contains_duplicate_statictest.cpp
@@ -6,47 +6,52 @@
 
 #include <array>
 
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include "forfun/contains_duplicate.hpp"
+
+TEMPLATE_TEST_CASE_SIG(
+    "Contains duplicate",
+    "[contains_duplicate]",
+    (auto contains_duplicate, contains_duplicate),
+    (forfun::contains_duplicate::adjacent_find_based::contains_duplicate<
+        std::array<int, 1>::iterator,
+        std::array<int, 1>::iterator>),
+    (forfun::contains_duplicate::quadratic::contains_duplicate<
+        std::array<int, 1>::iterator,
+        std::array<int, 1>::iterator>),
+    (forfun::contains_duplicate::sorted::contains_duplicate<
+        std::array<int, 1>::iterator,
+        std::array<int, 1>::iterator>),
+    (forfun::contains_duplicate::unordered_set_based::contains_duplicate<
+        std::array<int, 1>::iterator,
+        std::array<int, 1>::iterator>)
+)
+{
+    SECTION("One integer")
+    {
+        std::array nums{3};
+
+        REQUIRE_FALSE(contains_duplicate(nums.begin(), nums.end()));
+    }
+}
 
 TEST_CASE("Contains duplicate", "[contains_duplicate]")
 {
     using forfun::contains_duplicate::quadratic::contains_duplicate;
 
-    SECTION("One integer")
-    {
-        static constexpr std::array const nums{3};
-        static constexpr bool const expected{false};
-
-        static constexpr auto const actual{
-            contains_duplicate(nums.cbegin(), nums.cend())
-        };
-
-        STATIC_REQUIRE(actual == expected);
-    }
-
     SECTION("Three integers, one of which is unique")
     {
         static constexpr std::array const nums{19, 7, 19};
-        static constexpr bool const expected{true};
 
-        static constexpr auto const actual{
-            contains_duplicate(nums.cbegin(), nums.cend())
-        };
-
-        STATIC_REQUIRE(actual == expected);
+        STATIC_REQUIRE(contains_duplicate(nums.begin(), nums.end()));
     }
 
     SECTION("Three unique integers")
     {
         static constexpr std::array const nums{3, 2, 1};
-        static constexpr bool const expected{false};
 
-        static constexpr auto const actual{
-            contains_duplicate(nums.cbegin(), nums.cend())
-        };
-
-        STATIC_REQUIRE(actual == expected);
+        STATIC_REQUIRE_FALSE(contains_duplicate(nums.begin(), nums.end()));
     }
 }
