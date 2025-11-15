91ff4d73f2570630e48354dbcd4115a3ff76a400
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-11-16 00:45:18 +0200

    refactor: Make `constexpr`

--- include/forfun/subsets.hpp
+++ include/forfun/subsets.hpp
@@ -10,12 +10,59 @@
 #ifndef FORFUN_SUBSETS_HPP_
 #define FORFUN_SUBSETS_HPP_
 
+#include <iterator>
 #include <vector>
 
 namespace forfun::subsets {
 
-[[nodiscard]] auto explode_subsets(std::vector<int> const& elements)
-    -> std::vector<std::vector<int>>;
+namespace detail {
+
+template <
+    std::input_iterator ConstIter,
+    std::sentinel_for<ConstIter> Sentinel,
+    typename Container>
+constexpr auto do_explode_subsets(
+    std::vector<std::iter_value_t<ConstIter>> const& subset,
+    ConstIter const& first,
+    Sentinel const& last,
+    Container& subsets
+) -> void
+{
+    if (first == last) [[unlikely]]
+    {
+        subsets.emplace_back(subset);
+        return;
+    }
+
+    std::vector<std::iter_value_t<ConstIter>> current_subset{};
+    current_subset.reserve(subset.size() + 1UZ);
+
+    current_subset.assign(subset.cbegin(), subset.cend());
+
+    auto const next{std::next(first)};
+
+    do_explode_subsets(current_subset, next, last, subsets);
+
+    current_subset.emplace_back(*first);
+    do_explode_subsets(current_subset, next, last, subsets);
+}
+
+} // namespace detail
+
+[[nodiscard]] constexpr auto explode_subsets(std::vector<int> const& elements)
+    -> std::vector<std::vector<int>>
+{
+    std::vector<std::vector<int>> subsets{};
+    subsets.reserve(1UZ << elements.size());
+
+    std::vector<int> const empty_subset{};
+
+    detail::do_explode_subsets(
+        empty_subset, elements.cbegin(), elements.cend(), subsets
+    );
+
+    return subsets;
+}
 
 } // namespace forfun::subsets
 
