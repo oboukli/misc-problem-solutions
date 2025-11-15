6b0526e930fb4b4ea78367d98b5bd762cb9613c6
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-11-16 00:20:18 +0200

    refactor: Rename symbols

--- src/forfun/subsets.cpp
+++ src/forfun/subsets.cpp
@@ -19,29 +19,29 @@ template <
     typename Container>
 auto do_explode_subsets(
     std::vector<std::iter_value_t<ConstIter>>& subset,
-    ConstIter iter,
+    ConstIter first,
     Sentinel last,
     Container& subsets
 ) -> void
 {
-    if (iter == last) [[unlikely]]
+    if (first == last) [[unlikely]]
     {
         subsets.emplace_back(subset);
         return;
     }
 
-    std::vector<std::iter_value_t<ConstIter>> current_set{};
-    current_set.reserve(subset.size() + 1UZ);
+    std::vector<std::iter_value_t<ConstIter>> current_subset{};
+    current_subset.reserve(subset.size() + 1UZ);
 
-    current_set.assign(subset.cbegin(), subset.cend());
+    current_subset.assign(subset.cbegin(), subset.cend());
 
-    auto const element{*iter};
-    ++iter;
+    auto const element{*first};
+    ++first;
 
-    do_explode_subsets(current_set, iter, last, subsets);
+    do_explode_subsets(current_subset, first, last, subsets);
 
-    current_set.emplace_back(element);
-    do_explode_subsets(current_set, iter, last, subsets);
+    current_subset.emplace_back(element);
+    do_explode_subsets(current_subset, first, last, subsets);
 }
 
 } // namespace
@@ -52,11 +52,11 @@ auto do_explode_subsets(
     std::vector<std::vector<int>> subsets{};
     subsets.reserve(1UZ << elements.size());
 
-    std::vector<int> current_subset{};
-    current_subset.reserve(0UZ);
+    std::vector<int> empty_subset{};
+    empty_subset.reserve(0UZ);
 
     do_explode_subsets(
-        current_subset, elements.cbegin(), elements.cend(), subsets
+        empty_subset, elements.cbegin(), elements.cend(), subsets
     );
 
     return subsets;
