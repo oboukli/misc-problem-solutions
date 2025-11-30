31283873e6f5d2e60f840b2ff70c76b4ae4f094c
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-12-01 00:15:18 +0200

    refactor: Revisit Subsets interface

--- include/forfun/subsets.hpp
+++ include/forfun/subsets.hpp
@@ -10,6 +10,7 @@
 #ifndef FORFUN_SUBSETS_HPP_
 #define FORFUN_SUBSETS_HPP_
 
+#include <cstddef>
 #include <iterator>
 #include <vector>
 
@@ -26,16 +27,18 @@ namespace recursive {
 namespace detail {
 
 template <
-    std::input_iterator ConstIter,
-    std::sentinel_for<ConstIter> Sentinel,
+    typename ConstIter,
+    typename Sentinel,
     typename Container,
-    typename SubsetAllocator
-    = std::vector<std::iter_value_t<ConstIter>>::allocator_type>
+    typename ElementAllocator>
+    requires std::forward_iterator<ConstIter>
+    and std::sized_sentinel_for<ConstIter, Sentinel>
 constexpr auto do_explode_subsets(
-    std::vector<std::iter_value_t<ConstIter>> const& subset,
+    std::vector<std::iter_value_t<ConstIter>, ElementAllocator> const& subset,
     ConstIter const& first,
     Sentinel const& last,
-    Container& subsets
+    Container& subsets,
+    ElementAllocator const& element_allocator
 ) -> void
 {
     if (first == last) [[unlikely]]
@@ -44,35 +47,50 @@ constexpr auto do_explode_subsets(
         return;
     }
 
-    std::vector<std::iter_value_t<ConstIter>, SubsetAllocator> current_subset{};
+    std::vector<std::iter_value_t<ConstIter>, ElementAllocator> current_subset{
+        element_allocator
+    };
     current_subset.reserve(subset.size() + 1UZ);
 
     current_subset.assign(subset.cbegin(), subset.cend());
 
     auto const next{std::next(first)};
 
-    do_explode_subsets(current_subset, next, last, subsets);
+    do_explode_subsets(current_subset, next, last, subsets, element_allocator);
 
     current_subset.emplace_back(*first);
-    do_explode_subsets(current_subset, next, last, subsets);
+    do_explode_subsets(current_subset, next, last, subsets, element_allocator);
 }
 
 } // namespace detail
 
 template <
-    typename SubsetAllocator = std::vector<int>::allocator_type,
-    typename SetAllocator
-    = std::vector<std::vector<int, SubsetAllocator>>::allocator_type>
-[[nodiscard]] constexpr auto explode_subsets(std::vector<int> const& elements)
-    -> std::vector<std::vector<int>>
+    typename ConstIter,
+    typename Sentinel,
+    typename ElementAllocator = std::vector<int>::allocator_type,
+    typename SubsetAllocator
+    = std::vector<std::vector<int, ElementAllocator>>::allocator_type>
+    requires std::contiguous_iterator<ConstIter>
+    and std::sized_sentinel_for<ConstIter, Sentinel>
+[[nodiscard]] constexpr auto explode_subsets(
+    ConstIter const& first,
+    Sentinel const& last,
+    SubsetAllocator subset_allocator
+    = std::vector<std::vector<int>>::allocator_type{},
+    ElementAllocator element_allocator = std::vector<int>::allocator_type{}
+) -> std::vector<std::vector<int, ElementAllocator>, SubsetAllocator>
 {
-    std::vector<std::vector<int, SubsetAllocator>, SetAllocator> subsets{};
-    subsets.reserve(1UZ << elements.size());
+    std::vector<std::vector<int, ElementAllocator>, SubsetAllocator> subsets{
+        subset_allocator
+    };
+    subsets.reserve(
+        1UZ << static_cast<std::size_t>(std::abs(std::distance(first, last)))
+    );
 
-    std::vector<int, SubsetAllocator> const empty_subset{};
+    std::vector<int, ElementAllocator> const empty_subset{element_allocator};
 
     detail::do_explode_subsets(
-        empty_subset, elements.cbegin(), elements.cend(), subsets
+        empty_subset, first, last, subsets, element_allocator
     );
 
     return subsets;
