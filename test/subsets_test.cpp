1402604df38bcbc56ff4f4459a0dd596dade9721
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2025-12-01 00:20:18 +0200

    feat: Add Subsets in-place allocator factories

--- test/subsets_test.cpp
+++ test/subsets_test.cpp
@@ -40,6 +40,46 @@ TEST_CASE("Subsets", "[subsets]")
         REQUIRE(actual[1].capacity() == actual[1].size());
     }
 
+    SECTION("One element container with in-place allocator")
+    {
+        using ElementAllocatorFactory
+            = forfun::subsets::ElementInPlaceAllocatorFactory<int, 1>;
+        using SubsetAllocatorFactory
+            = forfun::subsets::SubsetInPlaceAllocatorFactory<
+                int,
+                ElementAllocatorFactory::allocator_type,
+                1>;
+
+        ElementAllocatorFactory const element_allocator_factory{};
+        SubsetAllocatorFactory const subset_allocator_factory{};
+
+        std::vector const elements{17};
+
+        std::vector<
+            std::vector<int, ElementAllocatorFactory::allocator_type>,
+            SubsetAllocatorFactory::allocator_type> const expected{{}, {17}};
+
+        CAPTURE(elements);
+
+        auto const actual{explode_subsets(
+            elements.cbegin(),
+            elements.cend(),
+            subset_allocator_factory.allocator(),
+            element_allocator_factory.allocator()
+        )};
+
+        REQUIRE_THAT(
+            actual,
+            Catch::Matchers::SizeIs(2UZ)
+                and Catch::Matchers::UnorderedRangeEquals(expected)
+        );
+
+        REQUIRE(actual.capacity() == 2UZ);
+
+        REQUIRE(actual.front().capacity() == actual.front().size());
+        REQUIRE(actual[1].capacity() == actual[1].size());
+    }
+
     SECTION("Two element container")
     {
         std::vector const elements{5, 7};
@@ -94,7 +134,7 @@ TEST_CASE("Subsets", "[subsets]")
         REQUIRE(actual[7].capacity() == actual[7].size());
     }
 
-    SECTION("Four element container")
+    SECTION("Four element container (benchmark case 1)")
     {
         std::vector const elements{23, 29, 31, 37};
 
@@ -147,6 +187,78 @@ TEST_CASE("Subsets", "[subsets]")
         REQUIRE(actual[14].capacity() == actual[14].size());
         REQUIRE(actual[15].capacity() == actual[15].size());
     }
+
+    SECTION("Four element container with in-place allocator (benchmark case 2)")
+    {
+        using ElementAllocatorFactory
+            = forfun::subsets::ElementInPlaceAllocatorFactory<int, 4>;
+        using SubsetAllocatorFactory
+            = forfun::subsets::SubsetInPlaceAllocatorFactory<
+                int,
+                ElementAllocatorFactory::allocator_type,
+                4>;
+
+        ElementAllocatorFactory const element_allocator_factory{};
+        SubsetAllocatorFactory const subset_allocator_factory{};
+
+        std::vector const elements{23, 29, 31, 37};
+
+        std::vector<
+            std::vector<int, ElementAllocatorFactory::allocator_type>,
+            SubsetAllocatorFactory::allocator_type> const expected{
+            {},
+            {23},
+            {29},
+            {31},
+            {37},
+            {23, 29},
+            {23, 31},
+            {23, 37},
+            {29, 31},
+            {29, 37},
+            {31, 37},
+            {23, 29, 31},
+            {23, 31, 37},
+            {23, 29, 37},
+            {29, 31, 37},
+            {23, 29, 31, 37}
+        };
+
+        CAPTURE(elements);
+
+        auto const actual{explode_subsets(
+            elements.cbegin(),
+            elements.cend(),
+            subset_allocator_factory.allocator(),
+            element_allocator_factory.allocator()
+        )};
+
+        REQUIRE_THAT(
+            actual,
+            Catch::Matchers::SizeIs(16UZ)
+                and Catch::Matchers::UnorderedRangeEquals(expected)
+        );
+
+        REQUIRE(actual.capacity() == 16UZ);
+
+        REQUIRE(actual.front().capacity() == actual.front().size());
+        REQUIRE(actual[1].capacity() == actual[1].size());
+        REQUIRE(actual[2].capacity() == actual[2].size());
+        REQUIRE(actual[3].capacity() == actual[3].size());
+        REQUIRE(actual[4].capacity() == actual[4].size());
+        REQUIRE(actual[5].capacity() == actual[5].size());
+        REQUIRE(actual[6].capacity() == actual[6].size());
+        REQUIRE(actual[7].capacity() == actual[7].size());
+
+        REQUIRE(actual[8].capacity() == actual[8].size());
+        REQUIRE(actual[9].capacity() == actual[9].size());
+        REQUIRE(actual[10].capacity() == actual[10].size());
+        REQUIRE(actual[11].capacity() == actual[11].size());
+        REQUIRE(actual[12].capacity() == actual[12].size());
+        REQUIRE(actual[13].capacity() == actual[13].size());
+        REQUIRE(actual[14].capacity() == actual[14].size());
+        REQUIRE(actual[15].capacity() == actual[15].size());
+    }
 }
 
 TEST_CASE("Subsets (degenerate case)", "[subsets]")
@@ -162,6 +274,45 @@ TEST_CASE("Subsets (degenerate case)", "[subsets]")
         CAPTURE(elements);
 
         auto const actual{explode_subsets(elements.cbegin(), elements.cend())};
+
+        REQUIRE_THAT(
+            actual,
+            Catch::Matchers::SizeIs(1UZ)
+                and Catch::Matchers::UnorderedRangeEquals(expected)
+        );
+
+        REQUIRE(actual.capacity() == 1UZ);
+
+        REQUIRE(actual.front().capacity() == actual.front().size());
+    }
+
+    SECTION("Empty container with in-place allocator")
+    {
+        using ElementAllocatorFactory
+            = forfun::subsets::ElementInPlaceAllocatorFactory<int, 1>;
+        using SubsetAllocatorFactory
+            = forfun::subsets::SubsetInPlaceAllocatorFactory<
+                int,
+                ElementAllocatorFactory::allocator_type,
+                0>;
+
+        ElementAllocatorFactory const element_allocator_factory{};
+        SubsetAllocatorFactory const subset_allocator_factory{};
+
+        std::vector<int> const elements{};
+
+        std::vector<
+            std::vector<int, ElementAllocatorFactory::allocator_type>,
+            SubsetAllocatorFactory::allocator_type> const expected{{}};
+
+        CAPTURE(elements);
+
+        auto const actual{explode_subsets(
+            elements.cbegin(),
+            elements.cend(),
+            subset_allocator_factory.allocator(),
+            element_allocator_factory.allocator()
+        )};
 
         REQUIRE_THAT(
             actual,
